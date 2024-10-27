9d4447135eea7ca934191d521f2c8586983d0fe6
nickolaykrasilov <krasilov.kolya@yandex.ru>
2024-10-27 17:34:28 +0300

    Добавление секции Контактов

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,13 @@
 import Image from "next/image";
 import Header from "@/components/header";
 import styles from "./page.module.css";
+import { Contacts } from "@/components/Contacts/contacts";
 
 export default function Home() {
   return (
     <div className={styles.page}>
       <Header></Header>
+      <Contacts /> 
     </div>
   );
 }
---
+++ src/components/Contacts/contacts.tsx
@@ -0,0 +1,45 @@
+import './style.css';
+import Image from 'next/image'; // Импорт компонент Image из next/image
+import image from './image.png'; // Импорт изображение из той же папки
+
+export function Contacts() {
+  return (
+    <div className="contact-section">
+      <div className="image-container">
+        <Image
+          src={image} // Используем импортированное изображение
+          alt="Изображение"
+          layout="responsive" // Устанавливаем адаптивный размер
+          width={800} // Ширина изображения
+          height={600} // Высота изображения
+        />
+      </div>
+    </div>
+  );
+}
+
+
+
+
+// export function Contacts() {
+//   return (
+//     <div className="contact-section">
+//       <h2>Контакты</h2>
+//       <div className="contact-info">
+//         <h3>Оптовый поставщик «Султан» </h3>
+//         <p>Адрес: ул. Примерная, 1, г. Примерск, 123456</p>
+//         <p>Телефон: +7 (123) 456-78-90</p>
+//         <p>Email: example@example.sfedu.ru</p>
+//       </div>
+//       <div className="map-container">
+//         <iframe
+//           title="Яндекс.Карта"
+//           src="https://yandex.ru/map-widget/v1/-/CDhYM2~3" 
+//           width="100%"
+//           height="400"
+//           frameBorder="0"
+//         ></iframe>
+//       </div>
+//     </div>
+//   );
+// }
