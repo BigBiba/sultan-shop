db76f70862adb5f712bf1a3fd53f556b5c450514
Kitayoza <juliapak032005@gmail.com>
2024-11-04 19:15:00 +0300

    added categories section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,13 +3,15 @@ import Header from "@/components/header";
 import styles from "./page.module.css";
 import { Contacts } from "@/components/Contacts/contacts";
 import PromItems from "@/components/Items/promItems";
+import Categories from "@/components/Product categories/categories";
 
 export default function Home() {
   return (
     <div className={styles.page}>
       <Header></Header>
       <PromItems />
-      <Contacts /> 
+      <Categories />
+      <Contacts />
     </div>
   );
 }
