743c34476433a4cfb4c90abd877f70094e69e5ca
nickolaykrasilov <krasilov.kolya@yandex.ru>
2024-11-14 02:12:58 +0300

    Адаптив футера и контакты сделанные по другому

--- src/components/Contacts/contacts.tsx
+++ src/components/Contacts/contacts.tsx
@@ -1,45 +1,25 @@
-import './style.css';
+import './contacts.css';
 import Image from 'next/image'; // Импорт компонент Image из next/image
 import image from './image.png'; // Импорт изображение из той же папки
+
 
 export function Contacts() {
   return (
     <div className="contact-section">
       <div className="image-container">
-        <Image
-          src={image} // Используем импортированное изображение
-          alt="Изображение"
-          layout="responsive" // Устанавливаем адаптивный размер
-          width={800} // Ширина изображения
-          height={600} // Высота изображения
-        />
+        <div className="blur-overlay">
+          <h2>Контакты</h2>
+          <div className="contact-info">
+            <h3>Оптовый поставщик «Султан»</h3>
+            <p>Адрес: г. Кокшетау, ул. Ж. Ташенова 129Б (Рынок Восточный)</p>
+            <p>Отдел продаж: +7 (777) 490-00-91</p>
+            <p>Email: opt.sultan@mail.ru</p>
+            <p>Данные налогоплательщика:</p>
+            <p>ИП Катран Д.С.</p>
+            <p>ИНН: 860113450858</p>
+          </div>
+        </div>
       </div>
     </div>
   );
 }
-
-
-
- 
-// export function Contacts() {
-//   return (
-//     <div className="contact-section">
-//       <h2>Контакты</h2>
-//       <div className="contact-info">
-//         <h3>Оптовый поставщик «Султан» </h3>
-//         <p>Адрес: ул. Примерная, 1, г. Примерск, 11221</p>
-//         <p>Телефон: +7 (123) 456-78-90</p>
-//         <p>Email: example@example.sfedu.ru</p>
-//       </div>
-//       <div className="map-container">
-//         <iframe
-//           title="Яндекс.Карта"
-//           src="https://yandex.ru/map-widget/v1/-/CDhYM2~3" 
-//           width="100%"
-//           height="400"
-//           frameBorder="0"
-//         ></iframe>
-//       </div>
-//     </div>
-//   );
-// }
