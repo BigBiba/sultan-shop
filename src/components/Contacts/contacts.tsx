a5ab862a6be99d88040d320eda21dcf462ce36a8
nickolaykrasilov <krasilov.kolya@yandex.ru>
2024-11-14 14:25:04 +0300

    Перевел в scss

--- src/components/Contacts/contacts.tsx
+++ src/components/Contacts/contacts.tsx
@@ -1,25 +1,42 @@
-import './contacts.css';
-import Image from 'next/image'; // Импорт компонент Image из next/image
-import image from './image.png'; // Импорт изображение из той же папки
+import './contacts.scss';
+import Image from 'next/image'; 
+import image from './image_1.png'; 
+
 
 
 export function Contacts() {
   return (
     <div className="contact-section">
       <div className="image-container">
-        <div className="blur-overlay">
-          <h2>Контакты</h2>
-          <div className="contact-info">
-            <h3>Оптовый поставщик «Султан»</h3>
-            <p>Адрес: г. Кокшетау, ул. Ж. Ташенова 129Б (Рынок Восточный)</p>
-            <p>Отдел продаж: +7 (777) 490-00-91</p>
-            <p>Email: opt.sultan@mail.ru</p>
-            <p>Данные налогоплательщика:</p>
-            <p>ИП Катран Д.С.</p>
-            <p>ИНН: 860113450858</p>
-          </div>
-        </div>
+        <Image
+          src={image} 
+          alt="Изображение"
+          layout="responsive" 
+          width={800} 
+          height={600} 
+        />
       </div>
     </div>
   );
 }
+
+// export function Contacts() {
+//   return (
+//     <div className="contact-section">
+//       <div className="image-container">
+//         <div className="blur-overlay">
+//           <h2>Контакты</h2>
+//           <div className="contact-info">
+//             <h3>Оптовый поставщик «Султан»</h3>
+//             <p>Адрес: г. Кокшетау, ул. Ж. Ташенова 129Б (Рынок Восточный)</p>
+//             <p>Отдел продаж: +7 (777) 490-00-91</p>
+//             <p>Email: opt.sultan@mail.ru</p>
+//             <p>Данные налогоплательщика:</p>
+//             <p>ИП Катран Д.С.</p>
+//             <p>ИНН: 860113450858</p>
+//           </div>
+//         </div>
+//       </div>
+//     </div>
+//   );
+// }
--- src/components/Footer/footer.tsx
+++ src/components/Footer/footer.tsx
@@ -1,4 +1,4 @@
-import './Footer.css'; 
+import './Footer.scss'; 
 import Image from 'next/image'; 
 import sultanLogo from './sultan.png';
 import visaLogo from './visa.png'; 
@@ -47,7 +47,6 @@ export function Footer () {
                 <h3>Скачать прайс-лист:</h3>
                 <a href="#" className="download-link">Прайс-лист</a>
                 <p className="messenger-info">Связь в мессенджерах:</p>
-                {/* Уменьшили отступ между строкой и иконками до 2px */}
                 <div className="messenger-logos" style={{ marginTop: '2px', display: 'flex', gap: '10px' }}>
                     <Image 
                         src={whatsappLogo} 
