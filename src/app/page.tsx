704b5f26027437abc1da5b2f6f493bbc3ed62f66
XD26 <anasmarova@gmail.com>
2024-11-15 18:59:35 +0300

    fix contacts adaptive

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,12 @@
 import Image from "next/image";
 import Header from "@/components/header";
-import styles from "./page.module.css";
 import { Contacts } from "@/components/Contacts/contacts";
 import { Footer } from "@/components/Footer/footer";
 
 
 export default function Home() {
   return (
-    <div className={styles.page}>
+    <div>
       <Header></Header>
       <Contacts /> 
       <Footer />
