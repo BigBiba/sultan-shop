202d977f9bec032ec3faaf8a94903a1a2e06df12
BigBiba <nikitafedorov.or@gmail.com>
2024-12-11 21:23:30 +0300

    feat(catalog) filter

--- src/app/catalog/[category]/page.tsx
+++ src/app/catalog/[category]/page.tsx
@@ -1,14 +1,47 @@
+'use client'
+
+import React, { useState } from 'react';
+
 import { notFound } from 'next/navigation';
-import Image from "next/image"
+import Image from "next/image";
 import { Item } from "@/public/models/Item";
-import {items} from "@/public/data/items"
+import {items} from "@/public/data/items";
+import ItemMini from "@/components/ItemMini/ItemMini";
+import styles from "./page.module.scss";
+import allCategories from "@/public/data/categories.json"
 
 export default function CatalogPage({params}: {params: {category: string}}) {
-  const category = params.category;
-    const itemsList = items.filter(item => item.categories && item.categories.includes(category));
+    const category = params.category;
+    const [selectedSubcategories, setSelectedSubcategories] = useState<string[]>([]);
+    const subcategories = allCategories.categories.find(ctgr => ctgr.category == category)?.subcategories.map(sub => sub.category);
+    const handleSubcategoryChange = (subcategory: string) => {
+      setSelectedSubcategories(prev => 
+        prev.includes(subcategory) 
+          ? prev.filter(item => item !== subcategory) 
+          : [...prev, subcategory]
+      );
+    };
+    const itemsList = items.filter(item =>
+       item.categories && item.categories.includes(category) &&
+       (selectedSubcategories.length === 0 || 
+        selectedSubcategories.some(subcategory => item.categories.includes(subcategory))));
     return (
-      <div>
-        
+      <div className={styles.item_container}>
+          {subcategories?.map(subcategory => (
+          <div key={subcategory}>
+            <label>
+              <input 
+                type="checkbox" 
+                checked={selectedSubcategories.includes(subcategory)} 
+                onChange={() => handleSubcategoryChange(subcategory)} 
+              />
+              {subcategory}
+            </label>
+          </div>
+          ))}
+          {itemsList.map(item => (
+            <ItemMini item={item}></ItemMini>
+          ))}
       </div>
     );
 }
